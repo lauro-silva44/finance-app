fae33c217fa69edf937ff6dc20893c13a29c5673
lauro <lauro@in8.com.br>
2022-12-24 14:39:00 -0100

    added the descriptin title and the button

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:finance_app/utlis/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,14 +21,65 @@ class HomePage extends StatelessWidget {
               'assets/images/girl-phone.png',
               height: 350,
             ),
-
             SizedBox(
-              child: Column(
-                children: [
-                  Text(
-                    'Easy way to manage'
-                  ),
-                ],
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  children: [
+                    Text(
+                      'Easy way to manage',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.archivo(
+                          color: Colors.black,
+                          fontSize: 35.0,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Container(
+                      color: myYellow,
+                      width: 240,
+                      height: 50,
+                      child: Text(
+                        'your money',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.archivo(
+                            color: Colors.black,
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                        width: 180,
+                        child: Text(
+                          'connect your money to friends & brands',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.archivo(
+                            color: descriptionColor,
+                            fontSize: 16,
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              minWidth: 241,
+              height: 53,
+              color: buttonColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child: Text(
+                'Get Started',
+                style: GoogleFonts.archivo(
+                  color: myYellow,
+                  fontSize: 18,
+                ),
               ),
             )
           ]),
--- lib/utlis/constants.dart
+++ lib/utlis/constants.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 const bcColor = Color(0xffFEF7F7);
+const buttonColor = Color(0xff2C3333);
 const myYellow = Color(0xffF7FF82);
 const myBlue = Color(0xffD1EFFE);
 const myPurple = Color(0xffDCB0FE);
+const descriptionColor = Color(0xff969596);
 const defaultPadding = 40.00;
